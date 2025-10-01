673b15b425b6f7dc65335ae0c09fd1dc34ae1ac0
komiakov <andrii.komiakov@icloud.com>
2025-10-01 20:18:46 +0300

    work with animation

--- app/components/hmnSidebar.vue
+++ app/components/hmnSidebar.vue
@@ -1,19 +1,46 @@
 <script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
+
 const { sidebarIsMinimal } = useSidebar()
 const icons = useIcons()
 const route = useRoute()
 
+const dropdownRef = ref(null)
+
+const topLinks = [
+    { to: '', icon: 'notification', label: 'Notifications' },
+    { type: 'divider' },
+    { to: '/', icon: 'dashboard', label: 'Dashboard' },
+    { to: '/databases', icon: 'database', label: 'Databases' },
+    { to: '/reports', icon: 'report', label: 'Reports' },
+]
+
+const bottomLinks = [
+    { to: '/license', icon: 'license', label: 'License' },
+    { type: 'divider' },
+    { to: '/documentation', icon: 'documentation', label: 'Documentation' },
+    { to: '/support', icon: 'support', label: 'Support' },
+    { type: 'divider' },
+    { to: '/settings', icon: 'settings', label: 'Settings' },
+]
+
+const closeSidebar = () => (sidebarIsMinimal.value = true)
+const openSidebar = () => (sidebarIsMinimal.value = false)
+
 const isActive = (path: string) => {
+    if (path == '') return false
     if ((path === '/' && route.path === '/') || (route.path.startsWith(path) && path !== '/')) {
         return true
     }
     return false
 }
 
+onClickOutside(dropdownRef, closeSidebar)
+
 </script>
 
 <template>
-    <nav class="navigation" :class="{ 'minimal': sidebarIsMinimal }">
+    <nav class="navigation" :class="{ 'minimal': sidebarIsMinimal }" ref="dropdownRef">
         <div class="navigation-top">
             <div id="navigation-header" class="container-flex">
                 <section>
@@ -23,47 +50,29 @@ const isActive = (path: string) => {
                     </NuxtLink>
                 </section>
                 <section>
-                    <HmnButton icon-left="close" @click="sidebarIsMinimal = !sidebarIsMinimal" />
+                    <HmnButton icon-left="close" @click="closeSidebar" />
                 </section>
             </div>
             <hr>
-            <NuxtLink to="" class="navigation-link">
-                <span class="icon" v-html="icons['notification']"></span>
-                <span class="navigation-link__label">Notifications</span>
-            </NuxtLink>
-            <hr>
-            <NuxtLink to="/" class="navigation-link" :class="{ 'active': isActive('/') }">
-                <span class="icon" v-html="icons['dashboard']"></span>
-                <span class="navigation-link__label">Dashboard</span>
-            </NuxtLink>
-            <NuxtLink to="/databases" class="navigation-link" :class="{ 'active': isActive('/databases') }">
-                <span class="icon" v-html="icons['database']"></span>
-                <span class="navigation-link__label">Databases</span>
-            </NuxtLink>
-            <NuxtLink to="/reports" class="navigation-link" :class="{ 'active': isActive('/reports') }">
-                <span class="icon" v-html="icons['report']"></span>
-                <span class="navigation-link__label">Reports</span>
-            </NuxtLink>
+            <template v-for="(link, index) in topLinks" :key="index">
+                <hr v-if="link.type === 'divider'" />
+                <NuxtLink v-else :to="link.to" class="navigation-link" :class="{ active: isActive(link.to!) }"
+                    @click="closeSidebar">
+                    <span class="icon" v-html="icons[link.icon!]"></span>
+                    <span class="navigation-link__label">{{ link.label }}</span>
+                </NuxtLink>
+            </template>
         </div>
+
         <div class="navigation-bottom">
-            <NuxtLink to="/license" class="navigation-link" :class="{ 'active': isActive('/license') }">
-                <span class="icon" v-html="icons['license']"></span>
-                <span class="navigation-link__label">License</span>
-            </NuxtLink>
-            <hr>
-            <NuxtLink to="/documentation" class="navigation-link" :class="{ 'active': isActive('/documentation') }">
-                <span class="icon" v-html="icons['documentation']"></span>
-                <span class="navigation-link__label">Documentation</span>
-            </NuxtLink>
-            <NuxtLink to="/support" class="navigation-link" :class="{ 'active': isActive('/support') }">
-                <span class="icon" v-html="icons['support']"></span>
-                <span class="navigation-link__label">Support</span>
-            </NuxtLink>
-            <hr>
-            <NuxtLink to="/settings" class="navigation-link" :class="{ 'active': isActive('/settings') }">
-                <span class="icon" v-html="icons['settings']"></span>
-                <span class="navigation-link__label">Settings</span>
-            </NuxtLink>
+            <template v-for="(link, index) in bottomLinks" :key="index">
+                <hr v-if="link.type === 'divider'" />
+                <NuxtLink v-else :to="link.to" class="navigation-link" :class="{ active: isActive(link.to!) }"
+                    @click="closeSidebar">
+                    <span class="icon" v-html="icons[link.icon!]"></span>
+                    <span class="navigation-link__label">{{ link.label }}</span>
+                </NuxtLink>
+            </template>
         </div>
     </nav>
 </template>
@@ -93,9 +102,9 @@ const isActive = (path: string) => {
     background: var(--bg-secondary);
     border-right: 1px solid var(--brdr-color);
 
-    box-shadow: 4px 0px 6px 0px rgba(0,0,0,0.15);
-    -webkit-box-shadow: 4px 0px 6px 0px rgba(0,0,0,0.15);
-    -moz-box-shadow: 4px 0px 6px 0px rgba(0,0,0,0.15);
+    box-shadow: 4px 0px 6px 0px rgba(0, 0, 0, 0.15);
+    -webkit-box-shadow: 4px 0px 6px 0px rgba(0, 0, 0, 0.15);
+    -moz-box-shadow: 4px 0px 6px 0px rgba(0, 0, 0, 0.15);
 
     &,
     .navigation-top,
@@ -208,7 +217,6 @@ const isActive = (path: string) => {
 @media (max-width: 959px) {
     .navigation {
         width: 260px;
-        transition: width .1s;
 
         &.minimal {
             left: -101vw;
