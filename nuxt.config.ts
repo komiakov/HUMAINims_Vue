08d858c35c0a60f04370ac5dfd7c53aac24e97b8
akomiakov <andrii.komiakov@icloud.com>
2025-10-07 13:29:00 +0300

    к

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' }
+      ]
+    }
+  },
+  
   css: ['~/assets/normalize.scss', '~/assets/globals.scss'],
 
   vite: {
