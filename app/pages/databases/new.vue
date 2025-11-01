69db05aa9062525c37818c60024e45c83f4dea1d
komiakov <andrii.komiakov@icloud.com>
2025-11-01 15:04:16 +0200

    update new db func panel

--- app/pages/databases/new.vue
+++ app/pages/databases/new.vue
@@ -5,17 +5,15 @@
 <template>
     <div id="functional-panel" class="container-flex">
         <section id="functional-panel__left">
+            <HmnButton icon-left="save" label="Save" type="accent" :disabled="true" />
             <HmnInput id="database-name" placeholder="Enter database name" />
         </section>
         <section>
             <HmnButton class="desktop-btns" icon-left="translator" type="primary" :disabled="true" />
             <HmnButton class="desktop-btns" icon-left="editForm" label="Edit form" type="secondary mbl-hide"
                 :disabled="true" />
-            <HmnButton class="desktop-btns" icon-left="save" label="Save" type="accent mbl-hide" :disabled="true" />
             <HmnDropdown class="mobile-btns" icon-right="more" type="secondary mbl-hide" id="header-dropdown">
                 <template #>
-                    <HmnButton icon-left="save" label="Save" type="accent" :disabled="true" />
-                    <hr>
                     <HmnButton icon-left="translator" label="Translations" type="primary" :disabled="true" />
                     <HmnButton icon-left="editForm" label="Edit form" type="primary" :disabled="true" />
                 </template>
